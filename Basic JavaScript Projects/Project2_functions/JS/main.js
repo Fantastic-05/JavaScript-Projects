5f0c8bdeaad1f2559af4a8332f55a009be39bffb
Fantastic-05 <hibakhan19@hotmail.com>
2026-02-20 22:06:40 -0500

    Project2_Function Assignment

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,10 @@
+function My_First_Function () {
+var str = "This is the button test!";
+document.getElementById("Button_Test").innerHTML = str;
+}
+
+function myFunction() {
+var sentence = "I am working";
+sentence += " a lot these days!";
+document.getElementById("Concatenate").innerHTML = sentence;
+}
