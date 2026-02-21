8dc5fb1ddbc9c56ea81f205ccca3ad233d8277e9
Fantastic-05 <hibakhan19@hotmail.com>
2026-02-20 23:29:42 -0500

    Project5_Comparisons_Type_coercion

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,51 @@
+document.write(typeof "Word");
+
+document.write("10" + 10);
+
+result1 = "5" + 5;
+console.log(result1); // '53'
+
+result2 = "5" - 2;
+console.log(result2); // 2
+
+document.write(2E310);
+
+document.write(-3E310);
+
+document.write(10 > 3);
+
+document.write(10 < 3);
+
+console.log(4 + 4);
+
+document.write(10 == 10);
+
+document.write(3 == 11);
+
+X = 10;
+Y = 10;
+document.write(X === Y);
+
+X = 82;
+Y = "82";
+document.write(X === Y);
+
+A = "Magnus";
+B = "Magnus";
+document.write(A === B);
+
+document.write(5 > 2 && 10 > 4);
+
+document.write(5 > 10 && 10 > 4);
+
+document.write(5 > 10 || 10 > 4);
+
+document.write(5 > 10 || 10 > 20);
+
+function not_Function() {
+document.getElementById("Not").innerHTML = !(20 > 10);
+}
+
+function not_Function() {
+document.getElementById("Not").innerHTML = !(5 > 10);
+}
